89393bbc051c9ed21d179e2c265bff1ef13eb58f
unknown <justiiturraspe@gmail.com>
2022-12-14 04:25:30 -0300

    cv and hover to navbar

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,16 +5,93 @@ function About() {
   return (
     
     <div className='about'>
-        <div className='curriculum'>
-            <h1>Jorgelina Parkinson</h1>
-            Mi entusiasmo por la pintura comienza en Paraná, Entre Ríos, a mis 20 años. En los talleres de Chana Naon y Amanda Mayor. Luego me traslado a Buenos Aires en el 1995 y concurro a los talleres de Marcelo Pellissier, Ernesto Pesce, Norberto Marcet, la academia de Guillermo Roux y arte sacro con Luis Quirós . Realicé escultura y cerámica.
-            Durante mucho tiempo me dedique a realizar bodegones y paisajes, tanto urbano como rural.
-            Defiendo una pintura de caballete, tonal, donde surge un lenguaje que transmite una idea, que debe llegar claramente al espectador a través de lo visual. Una pintura misteriosa y sugestiva.
-            Tengo tendencia a los tonos cálidos y terrosos. Paleta reducida, austera, invadida muy sutilmente por los tonos fríos.
-            He realizado muestras colectivas e individuales. Obtuve 1eros, 2dos y 3eros premios en Entre Ríos y en Santa Fé. Fui seleccionada en salones nacionales de las ciudades de Córdoba y Concordia. Convocada en dos oportunidades por la Universidad Nacional del Litoral para realizar retratos a personajes de nuestra historia.
-            Tengo un taller propio donde también dicto clases a mis alumnos, desde hace 13 años.
-        </div>
-        <img src='./bio.jpg' alt="" />    
+      <div className='curriculum'>
+        Desde 1987 comienza a realizar su proceso de aprendizaje con la prof.Chana Naon y luego con Amanda Mayor en Paraná.  
+        En 1995 se radica en Buenos Aires concurre al taller de Marcelo Pellisier, Ruben Grau y Jorge Garnica.  
+        Taller de Ernesto Pesce (1998)  
+        Conoce al maestro Guillermo Roux y profundiza sus estudios en su escuela. Conoce alli al discipulo Norberto Marcet, continua con el su aprendizaje.  
+        Concurre a Historia del Arte en la Asociacion Amigos del Museo Bellas Artes . Seminariodecrítica con López Anaya y Horacio Safons.  
+        Introducción al arte contemporáneo con Fabiana Barreda.  
+        Realiza escultura concurriendo al taller de Alberto del Ponte. 
+        <ul>
+          <li>
+          Luego en 2009-  se traslada a Paraná. Taller de Figura Humana en la facultad Católica de Sta. Fe. Concurre a Artes Visuales de Paraná donde continua sus estudios de Historia del Arte.  
+Seminario de Alugrafia y Poliéster con Natalia Giachetta. 
+          </li>
+          <li>
+            2013-  asiste al taller de Guillermo Roux. Luego en el 2014 nuevamente concurre al taller de Norberto Marcet.  
+          </li>
+          <li>
+            2015 -Realiza Encuentros de Reflexión y Pensamiento sobre Artistas Entrerrianos desde la contemporaneidad. Taller de Patrimonio Arquitectónico con la Arq. Mariana Melhem.
+          </li>
+          <li>
+            2017 – Curso de Arte Sacro dictado por  Luis Quirós, en la Universidad Católica de Santa Fe. Seminario de Dibujo plano, volumétrico y espacial en la UCSF.
+          </li>
+          <li>
+            2021 – Seminario de topografía de los lenguajes visuales con Nicolas Menza
+          </li>
+          <li>
+            2022 – Taller de Cerámica con Técnicas Prehispánicas con Karina Garret en Catamarca.
+          </li>
+        </ul>
+
+        <h2>Exposiciones y premios </h2>
+        <ul>
+          <li>
+          Desde 1995, comienza a exponer en Buenos Aires, Galería La Cave, 
+        Fundación Bollini, participa en muestras intercountries, en la Municipalidad de Pilar, en Estilo Pilar, Galería La Paz (CABA).     
+          </li>
+          <li>
+            Muestra individual en San Cristóbal (Rosario)  
+          </li>
+          <li>
+            Muestra colectiva en Central Newbery (2014- 2015-2016)
+          </li>
+          <li>
+            Muestra individual en UCSF. 
+          </li>
+          <li>
+            Muestra Inauguración Pabellón de las Artes de la UCA Pna.   
+          </li>
+          <li>
+            Muestra individual Galería AG (Sta. Fe) 2013- 2015 Participa de Salones Provinciales en Entre Ríos y Santa Fe.  
+          </li>
+          <li>
+            2016 Segundo Premio en el Salón Pcial de Entre Ríos.  
+          </li>
+          <li>
+            2016  Tercer Premio en la AAPS 
+          </li>
+          <li>
+            2017 Primer Premio Asosiaciòn Artistas Plàsticos Santafesinos y Municipalidad de Santa Fe.
+          </li>
+          <li>
+            2018 Muestra Casa de Gobierno de Entre Rìos.
+          </li>
+          <li>
+            2018 Muestra individual Galeria de Arte de la Càmara Federal de Entre Rios.
+          </li>
+          <li>
+            2018 Muestra colectiva en la Facultas de Derecho de la UBA.
+          </li>
+          <li>
+            2019 Primer Premio Salòn Provincial Entre Rios.
+          </li>
+          <li>
+            2019 Muestra Colectiva en la Facultad de Derecho de la UBA.
+          </li>
+          <li>
+            2019 Muestra colectiva en GAP Paranà.
+          </li>
+          <li>
+            2021 Seleccionada en el Salón del Bicentenario de MAV Concordia
+          </li>
+          <li>
+            2021 Seleccionada en el Salón BANCOR de la Pcia de Cordoba
+          </li>
+        </ul>
+      </div>
+      <img src='./bio.jpg' alt="" className='photoCv'/>
     </div>
     
   )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,21 +2,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 function Header() {
   return (
     <Navbar expand="lg" className='navbar'>
       <Container>
-        <NavLink to="/"><p className='title'>Jorgelina Parkinson</p></NavLink>
+        <NavLink className='title' to="/">Jorgelina Parkinson</NavLink>
         
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className='rightNavbarContainer'>
           <Nav className='rightNavbar'>
             <NavLink className='navbarItems' to="/">Inicio</NavLink>
             <NavLink className='navbarItems' to="/about">Bio</NavLink>
-            <NavDropdown className='navbarItems' title="Obras" id="basic-nav-dropdown">
+            <NavDropdown title="Obras" id="basic-nav-dropdown">
               <NavDropdown.Item href="/about">Paisajes</NavDropdown.Item>
               <NavDropdown.Item href="/">Mujeres SXX</NavDropdown.Item>
               <NavDropdown.Item href="/">Naturaleza muerta</NavDropdown.Item>
