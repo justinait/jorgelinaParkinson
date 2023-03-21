5ce63f179af017c8df41bee85b0a5e820637dea4
justinait <justiiturraspe@gmail.com>
2023-03-21 17:43:48 -0300

    css focus black

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -12,10 +12,6 @@ function CarouselFade() {
             src='./paintings/bridges-ports/ports (19).jpg'
             alt="First slide"
           />
-          {/* <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
         </Carousel.Item>
         <Carousel.Item className='carouselItem'>
           <img
@@ -23,10 +19,6 @@ function CarouselFade() {
             src='./paintings/women/women (6).jpg'
             alt="Second slide"
           />
-          {/* <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption> */}
         </Carousel.Item>
         <Carousel.Item className='carouselItem'>
           <img
@@ -34,12 +26,6 @@ function CarouselFade() {
             src='./paintings/landscape/landscape (2).jpg'
             alt="Third slide"
           />
-          {/* <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption> */}
         </Carousel.Item>
       </Carousel>
 
